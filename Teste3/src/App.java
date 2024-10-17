8db32fad339fb8218bdee58ae24fc68d56a105b7
NicolasAlvess <nicolasoliveira.alves@gmail.com>
2024-10-16 21:02:19 -0300

    Primeiro commit com visual code

---
+++ Teste3/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("teste de commit");
+        System.out.println("hajhdkjhsahdoihosahdoiodihal");
+    }
+}
